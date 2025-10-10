e4b51c666a2a8c37ae7a852127fec6d325ed4704
Zacarias King Zaca <zacariaseduardoj@gmail.com>
2025-10-10 16:21:22 +0100

    novo

---
+++ CursoJs/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var v = 100
+console.log(`A velocidade do seu carro é ${v}km/h`)
+if (v > 60) {
+    console.log("Você foi multado por excesso de velocidade")
+}
+console.log("Dirija sempre com cinto de seguraça.")
---
+++ CursoJs/Aula11/ex009.js
@@ -0,0 +1,6 @@
+var p = "EUA"
+if (p == "Brasil") {
+    console.log("Brasileiro")
+} else {
+    console.log("Estrangeiro!")
+}
